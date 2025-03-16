e1f9e6bcad2672e58f0c66ae9ddbbf24213cd709
Alan Chen <alrichen@ucsc.edu>
2025-03-16 03:13:47 -0700

    make getElementById an arrow function and call it properly

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const $ = function (id) { return document.getElementById(id); };
+const $ = (id) => { return document.getElementById(id); };
 window.addEventListener('load', () => {
     $("empForm").addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log(`ID: ${id.value}`)
-        console.log(`Name: ${name.value}`)
-        console.log(`Extension: ${ext.value}`)
-        console.log(`Email: ${email.value}`)
-        console.log(`Department: ${department.value}`)
+        // const form = event.target;
+        console.log(`ID: ${$("id").value}`)
+        console.log(`Name: ${$("name").value}`)
+        console.log(`Extension: ${$("ext").value}`)
+        console.log(`Email: ${$("email").value}`)
+        console.log(`Department: ${$("department").value}`)
     })
 });
