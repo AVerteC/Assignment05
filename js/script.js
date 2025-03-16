ee6394130f8c6ea3f4ad6d8bb2203eca6b539fcd
Alan Chen <alrichen@ucsc.edu>
2025-03-16 03:16:53 -0700

    could also use event.target.field.value but using document.getElementById now

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const $ = (id) => { return document.getElementById(id); };
 window.addEventListener('load', () => {
     $("empForm").addEventListener("submit", (event) => {
         event.preventDefault();
-        // const form = event.target;
         console.log(`ID: ${$("id").value}`)
         console.log(`Name: ${$("name").value}`)
         console.log(`Extension: ${$("ext").value}`)
