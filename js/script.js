484e4baac4ec5f39d790731c541fbae9d4c9d768
Alan Chen <alrichen@ucsc.edu>
2025-03-16 03:03:31 -0700

    tried assignment

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const $ = function (id) { return document.getElementById(id); };
+window.addEventListener('load', () => {
+    $("empForm").addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(`ID: ${id.value}`)
+        console.log(`Name: ${name.value}`)
+        console.log(`Extension: ${ext.value}`)
+        console.log(`Email: ${email.value}`)
+        console.log(`Department: ${department.value}`)
+    })
+});
